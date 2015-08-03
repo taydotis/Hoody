2cb3bdfc3e0fd2ef7761727d54fd4cefada1207c
Taylor Raboin <hello@tay.is>
2015-08-03 14:10:48 -0500

    removed insecure and allowed certain writes to posts

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,13 +1,13 @@
 Template.postSubmit.events({
-	'submit form': function(e) {
-		e.preventDefault();
+  'submit form': function(e) {
+    e.preventDefault();
 
-		var post = {
-			url: $(e.target).find('[name=url]').val(),
-			title: $(e.target).find('[name=title]').val()
-		};
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
 
-		post._id = Posts.insert(post);
-		router.go('postPage', post);
-	}
+    post._id = Posts.insert(post);
+    Router.go('postPage', post);
+  }
 });
